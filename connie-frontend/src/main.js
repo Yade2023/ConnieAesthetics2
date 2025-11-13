581e1c90fff2f04ac4125d4d7ffd0542970c62c6
Yade2023 <144205116+Yade2023@users.noreply.github.com>
2025-11-13 12:57:31 +0800

    原始碼搬移
    原始碼搬移

--- connie-frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/css/main.css'
-import './assets/css/global.css'
-import './assets/css/home.css';
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
